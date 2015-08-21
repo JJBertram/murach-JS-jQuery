1fe3c7a2d9c721cf512aac5ec1c64efaaa7ed485
James Bertram <JamesBertram@tutanota.com>
2015-08-21 14:31:33 -0700

    slide show using jquery

--- slideshow/slideshow.js
+++ slideshow/slideshow.js
@@ -1,35 +1,25 @@
-(function(){
-var $ = function(id){
-	return document.getElementById(id);
-}
-
-window.onload = function()
-{
-	var imageList = $("image_list").getElementsByTagName("a");
-	var imageNode = $("image");
-	var output = $("output");
-	var imageCache = [];
-
-	//preprocess images
-	var i, image, link, timer;
-	for(i = 0; i < imageList.length; i++)
-	{
-		link = imageList[i];
-		image = new Image();
-		image.src = link.href;
-		image.title = link.title;
-		imageCache.push(image);
-	}
-	//slideshow timer
-	var imageCounter = 0;
-	var timer = setInterval(
-		function()
+$(function(){
+	var nextSlide = $("#slides img:first-child");
+	var nextCaption;
+	var nextSlideSrc;
+	setInterval(function ()
 		{
-			imageCounter = (imageCounter + 1)%imageCache.length;
-			image = imageCache[imageCounter];
-			imageNode.src = image.src;
-			output.firstChild.nodeValue = image.title;
-		}, 2000 );
-}
+			$("#output").fadeOut(1000);
+			$("#image").fadeOut(1000, function() 
+				{
+					if(nextSlide.next().length == 0) {
+						nextSlide = $("#slides img:first-child");
+					}
+					else {
+						nextSlide = nextSlide.next();
+					}
+					nextSlideSrc = nextSlide.attr("src");
+					nextCaption = nextSlide.attr("alt");
+					$("#image").attr("src", nextSlideSrc).fadeIn(1000);
+					$("#output").text(nextCaption).fadeIn(1000);
+				}//end image fadeout callback
+			)//close image fadeout ()
+		}, 
+	3000);//end setInterval function param
 
-})();
+})//end ready
